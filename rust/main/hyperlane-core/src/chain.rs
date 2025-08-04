c00aed0c52bad79a7bb01af7eeac38b9cda2a1aa
ross-weir <29697678+ross-weir@users.noreply.github.com>
2025-08-04 18:12:31 +1000

    add sovereign sdk integration rust

--- rust/main/hyperlane-core/src/chain.rs
+++ rust/main/hyperlane-core/src/chain.rs
@@ -412,6 +412,8 @@ pub enum HyperlaneDomainProtocol {
     CosmosNative,
     /// A Raidx based chain
     Radix,
+    /// A Sovereign-based chain type which uses hyperlane-sovereign.
+    Sovereign,
 }
 
 impl HyperlaneDomainProtocol {
@@ -774,7 +776,7 @@ impl HyperlaneDomain {
         use HyperlaneDomainProtocol::*;
         let protocol = self.domain_protocol();
         match protocol {
-            Ethereum | Cosmos | CosmosNative | Starknet => IndexMode::Block,
+            Ethereum | Cosmos | CosmosNative | Starknet | Sovereign => IndexMode::Block,
             Fuel | Sealevel | Radix => IndexMode::Sequence,
         }
     }
@@ -947,6 +949,7 @@ mod tests {
             ("cosmosnative", HyperlaneDomainProtocol::CosmosNative) => {}
             ("ethereum", HyperlaneDomainProtocol::Ethereum) => {}
             ("sealevel", HyperlaneDomainProtocol::Sealevel) => {}
+            ("sovereign", HyperlaneDomainProtocol::Sovereign) => {}
             ("starknet", HyperlaneDomainProtocol::Starknet) => {}
             _ => {
                 panic!(
